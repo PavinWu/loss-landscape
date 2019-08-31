bfa842ff34b70090f5392f957c72a5cc610705b3
PavinWu <pavin.wu@gmail.com>
2019-08-31 17:48:54 +1000

    add cifar10 to models

--- cifar10/model_loader.py
+++ cifar10/model_loader.py
@@ -1,8 +1,14 @@
 import os
 import torch, torchvision
+"""
+# if use with cifar10 as working directory
 import models.vgg as vgg
 import models.resnet as resnet
 import models.densenet as densenet
+"""
+import cifar10.models.vgg as vgg
+import cifar10.models.resnet as resnet
+import cifar10.models.densenet as densenet
 
 # map between model name and function
 models = {

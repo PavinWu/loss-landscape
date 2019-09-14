d22b91605e7e2e662194e7ca34eba331ac827981
PavinWu <pavin.wu@gmail.com>
2019-09-14 19:00:55 +1000

    Fixing (keep constant) nw_inplane instead of num_plane & Use last boundary weights when x falls out of range

--- subplanes.py
+++ subplanes.py
@@ -1,5 +1,8 @@
 import h5py
 
+def x_coord(elem):
+    return elem[1]
+
 def get_b_list(num_plane, nw_inplane, nw_inplane_left, pjt_list_sorted, print_lists):
     """
     Get a list of x-axis boundary of each subplane, for all subplanes
@@ -13,15 +16,21 @@ def get_b_list(num_plane, nw_inplane, nw_inplane_left, pjt_list_sorted, print_li
     """
     b_list = []
 
-    nw_extra = 0
+    nw = nw_inplane
     i_max = None    # i of sorted list, containing max index in subplane of original list 
     i = 0
-    for ip in range(0, num_plane):
-        if ip > num_plane - nw_inplane_left - 1:    # add extra to last
+    extra_plane = 0
+    if nw_inplane_left > 0:
+        extra_plane = 1
+    for ip in range(0, num_plane+extra_plane):
+        """
+        if ip > num_plane - num_plane_left - 1:    # add extra to last
             nw_extra = 1
-            
+        """
+        if ip == num_plane:
+            nw = nw_inplane_left    
         mono_ascend = True
-        for iw in range(0, nw_inplane+nw_extra):
+        for iw in range(0, nw):
             if iw == 0:
                 if ip == 0:
                     i_max = 0
@@ -48,17 +57,19 @@ def get_b_list(num_plane, nw_inplane, nw_inplane_left, pjt_list_sorted, print_li
                 print("i=", i, "b_list index=", j, elem)
             print()
 
+    if b_list[0][1] > b_list[-1][1]:    # b_list descends as index increase
+        b_list = sorted(b_list, key=x_coord)    # reverse (make ascend): x always ascend when finding loss for plot
+
     return b_list
 
-def x_coord(elem):
-    return elem[1]
-
-def boundary_list(projtraj_file, num_plane, num_w_per_pca):
-    
+def boundary_list(projtraj_file, nw_inplane, num_w_per_pca):
+    # num plane is number of subplane, NOT PCA plane
+    """ # for testing
     projtraj_file = '/home/uvin/trained_nets/resnet56_sgd_lr=0.1_bs=64_wd=0.0005_mom=0.9_save_epoch=3/PCA_weights_save_epoch=3/directions_iter_0.h5_iter_0_proj_cos.h5'
     #num_plane = 14
     num_w_per_pca = 14
-    
+    """
+
     print_lists = False
 
     f = h5py.File(projtraj_file, 'r')
@@ -66,13 +77,13 @@ def boundary_list(projtraj_file, num_plane, num_w_per_pca):
         f.close()
         print("Cannot find projected x and/or y coordinates in " + projtraj_file)
         return None        
-    if num_plane > num_w_per_pca + 1:
+    if nw_inplane > num_w_per_pca + 1:
         print("Number of subplane must be less than or equal to number of weights per PCA")
         return None
 
     l = len(f['proj_xcoord'])
-    nw_inplane = l//num_plane   # number of weights in a subplane
-    nw_inplane_left = l%num_plane
+    num_plane = l//nw_inplane   # number of weights in a subplane
+    nw_inplane_left = l%nw_inplane
 
     pjt_list = [[i,x,y] for i,x,y in zip(range(l), f['proj_xcoord'], f['proj_ycoord'])]
     
@@ -95,19 +106,20 @@ def boundary_list(projtraj_file, num_plane, num_w_per_pca):
 
 def locate_boundary(x, b_list, save_epoch):
     i = 0
-    l = len(b_list[0])
+    l = len(b_list)
+
     while i < l and x > b_list[i][1]:
         # linear search: slow, but doesn't matter much (b_list is quite small (at most a hundred something))
         i += 1
 
-    if i == 0:
-        xl, xr = None, b_list[0][1]
-        yl, yr = None, b_list[0][2]
-        iwl, iwr = None, b_list[0][0]*save_epoch
-    elif i == l:
-        xl, xr = b_list[l-1][1], None
-        yl, yr = b_list[l-1][2], None
-        iwl, iwr = b_list[l-1][0]*save_epoch, None
+    if i == 0:  # extend leftmost plane
+        xl, xr = b_list[0][1], b_list[1][1]
+        yl, yr = b_list[0][2], b_list[1][2]
+        iwl, iwr = b_list[0][0]*save_epoch, b_list[1][0]*save_epoch
+    elif i == l:    # extend rightmost plane
+        xl, xr = b_list[l-2][1], b_list[l-1][1]
+        yl, yr = b_list[l-2][2], b_list[l-1][2]
+        iwl, iwr = b_list[l-2][0]*save_epoch, b_list[l-1][0]*save_epoch
     else:
         xl, xr = b_list[i-1][1], b_list[i][1]
         yl, yr = b_list[i-1][2], b_list[i][2]

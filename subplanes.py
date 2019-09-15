624728490d6a14e92e319047df51e4f41bfdc687
PavinWu <pavin.wu@gmail.com>
2019-09-15 20:28:16 +1000

    Code to get boundaries of all x of a plane & and compare the subplanes with cosine similarity

--- subplanes.py
+++ subplanes.py
@@ -1,4 +1,6 @@
 import h5py
+import net_plotter
+import projection
 
 def x_coord(elem):
     return elem[1]
@@ -107,7 +109,6 @@ def boundary_list(projtraj_file, nw_inplane, num_w_per_pca):
 def locate_boundary(x, b_list, save_epoch):
     i = 0
     l = len(b_list)
-
     while i < l and x > b_list[i][1]:
         # linear search: slow, but doesn't matter much (b_list is quite small (at most a hundred something))
         i += 1
@@ -115,7 +116,7 @@ def locate_boundary(x, b_list, save_epoch):
     if i == 0:  # extend leftmost plane
         xl, xr = b_list[0][1], b_list[1][1]
         yl, yr = b_list[0][2], b_list[1][2]
-        iwl, iwr = b_list[0][0]*save_epoch, b_list[1][0]*save_epoch
+        iwl, iwr = b_list[0][0]*save_epoch, b_list[1][0]*save_epoch     # TODO this only works if index of point corresponds 1-to-1 to epoch number. Not the case if also use bc!
     elif i == l:    # extend rightmost plane
         xl, xr = b_list[l-2][1], b_list[l-1][1]
         yl, yr = b_list[l-2][2], b_list[l-1][2]
@@ -126,3 +127,88 @@ def locate_boundary(x, b_list, save_epoch):
         iwl, iwr = b_list[i-1][0]*save_epoch, b_list[i][0]*save_epoch
 
     return xl, xr, yl, yr, iwl, iwr
+
+def get_weights_from_iwnet(args, iw):
+    net = model_loader.load(args.dataset, args.model, net_plotter.get_model_name(args.model_folder, iw))
+    return net_plotter.get_weights(net)
+
+def compare_subplanes(args, bound_file, directions):
+    """ Get cosine similarities of and distances from all subplanes in a PCA plot """ 
+    """ To be used with plot_surface.py (needed to load d)"""
+    """ Didn't do this directly in plot_surface.py because .. forgot """
+
+    angles = [[]]
+    xs = [[]]
+    dx = directions[0]
+    dy = directions[1]
+    cl, cr = 0.5, 0.5   
+    step = [0, 0]       
+
+    for i, x_col in enumerate(b_dict['x']):
+        xl, xr = b_dict['xl'][i], b_dict['xr'][i]
+        yl, yr = b_dict['yl'][i], b_dict['yr'][i]
+        iwl, iwr = b_dict['iwl'][i], b_dict['iwr'][i]
+        wl, wr = get_weights_from_iwnet(iwl), get_weights_from_iwnet(iwr)
+
+        newstep = np.array([0, 0], dtype=np.float64)
+        newstep[0], newstep[1] = (step[0]-cl*xl-cr*xr), (step[1]-cl*yl-cr*yr)
+        changes_col = [(d0*newstep[0] + d1*newstep[1]) + (wl0*cl + wr0*cr).numpy() for (d0, d1, wl0, wr0) in zip(dx, dy, wl, wr)]
+        
+        for j, x_row in enumerate(b_dict['x'][i+1:]):     # symmetric
+            xl, xr = b_dict['xl'][j], b_dict['xr'][j]
+            yl, yr = b_dict['yl'][j], b_dict['yr'][j]
+            iwl, iwr = b_dict['iwl'][j], b_dict['iwr'][j]
+            wl, wr = get_weights_from_iwnet(iwl), get_weights_from_iwnet(iwr)
+            
+            newstep = np.array([0, 0], dtype=np.float64)
+            newstep[0], newstep[1] = (step[0]-cl*xl-cr*xr), (step[1]-cl*yl-cr*yr)
+            changes_row = [(d0*newstep[0] + d1*newstep[1]) + (wl0*cl + wr0*cr).numpy() for (d0, d1, wl0, wr0) in zip(dx, dy, wl, wr)]
+
+            xs[i].append((x_col, x_row))
+            angle[i].append(cal_angle(changes_col, changes_row))
+        
+        xs.append([])
+        angles.append([])
+    
+    bound_dict_keys = ['x', 'xl', 'xr', 'yl', 'yr', 'iwl', 'iwr']
+    bound_dict = {k: [] for k in bound_dict_keys}
+    f = h5py.File(bound_file, 'r')
+    for k in bound_dict_keys:
+        bound_dict[k] = f[k]
+    f.close()
+
+    comp_file = bound_file[:-3] + 'compare_plane.h5'
+    f = h5py.File(comp_file, 'w')
+    f['xs'] = xs
+    f['angles'] = angles
+    f.close()
+
+    return comp_file    
+
+
+def boundaries_dict(args, b_list, surf_file):
+    """ Get boundaries dict of all x-value of the plot """   
+    #not tested 
+    try:
+        args.xmin, args.xmax, args.xnum = [float(a) for a in args.x.split(':')]
+        xcoordinates = np.linspace(args.xmin, args.xmax, num=args.xnum)
+    except:
+        raise Exception('Improper format for x- or y-coordinates. Try something like -1:1:51')
+
+    bound_dict_keys = ['x', 'xl', 'xr', 'yl', 'yr', 'iwl', 'iwr']
+    bound_dict = {k: [] for k in bound_dict_keys}
+    
+    b_len = 6
+    for x in xcoordinates:
+        bound_details = locate_boundary(x, b_list, args.save_epoch)
+        bound_dict['x'].append(x)
+        for k, b in zip(bound_dict_keys[1:], bound_details):
+            bound_dict[k].append(b)
+
+    bound_file = surf_file[:-3] + 'boundaries.h5'       # just use surf_file for name
+    f = h5py.File(bound_file, 'w')
+    for k in bound_dict_keys:
+        f[k] = bound_dict[k]
+    f.close()
+
+    return bound_file
